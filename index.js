91b7009a0a1626264346f060e83a03f013608fa6
avdhutbhakare <141303447+avdhutbhakare@users.noreply.github.com>
2025-01-07 12:51:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var randomNumber1= Math.floor(Math.random()*3)+1;
-var image1= "images/Image" + randomNumber1 + ".png";
+var image1= "Image" + randomNumber1 + ".png";
 document.querySelectorAll("img")[0].setAttribute("src", image1);
 
 var randomNumber2 = Math.floor(Math.random() * 3) + 1;
-var image2 = "images/Image" + randomNumber2 + ".png";
+var image2 = "Image" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", image2);
 
 var resultText = ""
